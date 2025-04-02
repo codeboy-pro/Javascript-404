2a319a28bacc6ae58ee1fb72363f6b107092da42
Pradip Maity <pradipmaity9907@gmail.com>
2025-04-02 20:53:40 +0530

    Add files via upload

---
+++ JS_BASICS/03.js
@@ -0,0 +1,25 @@
+// let someNumber=33;
+// let stringNumber= String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// console.log(2+1);
+// console.log(2-1);
+// console.log(2/1);
+// console.log(2%1);
+// console.log(2*1);
+// console.log(2**2);
+
+
+let str1="Pradip";
+let str2=" Maity";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1" +2+2);
+console.log(Number("1") +2+2);
+
+
+
+console.log(+true);
+console.log(+" ");
---
+++ JS_BASICS/2.js
@@ -0,0 +1,15 @@
+// let score= true;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let num=Number(score);
+// console.log(typeof(num));
+// console.log(num);
+
+
+//"77"=> 77
+//"44asd"=> NAN
+let hui="klo";
+
+let boo= Boolean(hui);
+console.log(boo);
