77dc3885f5d0a5a94c85fdc6f77ff80109e01586
Pradip Maity <pradipmaity9907@gmail.com>
2025-04-02 21:06:49 +0530

    Add files via upload

---
+++ controle_flow/controle_flow.js
@@ -0,0 +1,36 @@
+//if 
+
+
+// const isUserloggedIn=true;
+
+// if(2 !=2){
+//     console.log("executed");
+
+// }
+// else{
+//     console.log("Not executed");
+// }
+
+// 2<=2,3!=2,2==='2'
+// < ,>,<=,>=,==,!=,===
+
+// const score=200;
+// if(score>100){
+//     const power="fly";
+//     console.log(`user power: ${power}`);
+
+// }
+
+
+
+
+const balance=1000;
+if(balance>500){
+    console.log("test");
+
+
+}
+if(balance>500)  console.log("test"),console.log("test2");
+
+
+
